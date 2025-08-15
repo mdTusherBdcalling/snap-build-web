cf8841b2f5d020dc7f489b13a0f1a78d55f8f5eb
mdTusherBdcalling <mdtusher.bdcalling@gmail.com>
2025-08-15 14:04:29 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/snap-build-web/",
+  base: "/snap-build-web/", // 👈 Add this line (must match your repo name)
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
